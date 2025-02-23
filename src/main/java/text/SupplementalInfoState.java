04a09af25c832b9e7b625b00846c671bbc5f56f7
John Serock <1468970+serock@users.noreply.github.com>
2025-02-22 19:28:55 -0500

    Add tax-exempt dividend info

--- src/main/java/text/SupplementalInfoState.java
+++ src/main/java/text/SupplementalInfoState.java
@@ -12,7 +12,7 @@ class SupplementalInfoState implements State {
     private static final Pattern patternPercent = Pattern.compile("[0-9]{1,3}\\.[0-9]{2}%?");
     private static final Pattern patternStartsWithFedSourceTotal = Pattern.compile("^Fed Source Total");
     private static final Pattern patternStartsWithFgnSourceIncTot = Pattern.compile("^Fgn Source Inc Tot");
-    private static final Pattern patternStartsWithStateAndPercentage = Pattern.compile("^[A-Z][A-Za-z. ]{2,16}[a-z]\\s[0-9]{1,3}\\.[0-9]{2}");
+    private static final Pattern patternStateAndPercentage = Pattern.compile("([A-Z][A-Za-z. ]{2,16}[a-z])\\s+([0-9]{1,3}\\.[0-9]{2})");
     private static final Pattern patternUSGOPercentage = Pattern.compile("the U.S government obligation percentage is");
     private static final Pattern patternWhitespace = Pattern.compile("\\s+");
 
@@ -69,7 +69,7 @@ class SupplementalInfoState implements State {
             final String[] lastRow = context.getLastSupplementalInfoRow();
             lastRow[Constants.SI_FIELD_SOURCE] = "Fed Source Total";
             matcher = patternPercent.matcher(text);
-            final String percent = matcher.find(end) ? cleanPercent(matcher.group()) : "";
+            final String percent = matcher.find(end) ? matcher.group() : "";
             lastRow[Constants.SI_FIELD_PERCENTAGE] = percent;
             return;
         }
@@ -79,7 +79,7 @@ class SupplementalInfoState implements State {
             String[] lastRow = context.getLastSupplementalInfoRow();
             lastRow[Constants.SI_FIELD_SOURCE] = "Fgn Source Inc Tot";
             matcher = patternPercent.matcher(text);
-            String percent = matcher.find(end) ? cleanPercent(matcher.group()) : "";
+            String percent = matcher.find(end) ? matcher.group() : "";
             lastRow[Constants.SI_FIELD_PERCENTAGE] = percent;
             matcher = patternFgnSourceIncQual.matcher(text);
             if (matcher.find()) {
@@ -89,7 +89,7 @@ class SupplementalInfoState implements State {
                 row[Constants.SI_FIELD_SECURITY_DESCRIPTION] = lastRow[Constants.SI_FIELD_SECURITY_DESCRIPTION];
                 row[Constants.SI_FIELD_SOURCE] = "Fgn Source Inc Qual";
                 matcher = patternPercent.matcher(text);
-                percent = matcher.find(end) ? cleanPercent(matcher.group()) : "";
+                percent = matcher.find(end) ? matcher.group() : "";
                 row[Constants.SI_FIELD_PERCENTAGE] = percent;
                 context.addSupplementalInfoRow(row);
                 row[Constants.SI_FIELD_AMOUNT] = getOrdinaryDividendsAmountFormula(context);
@@ -102,7 +102,7 @@ class SupplementalInfoState implements State {
                 row[Constants.SI_FIELD_SECURITY_DESCRIPTION] = lastRow[Constants.SI_FIELD_SECURITY_DESCRIPTION];
                 row[Constants.SI_FIELD_SOURCE] = "Fgn Source Inc Adj";
                 matcher = patternPercent.matcher(text);
-                percent = matcher.find(end) ? cleanPercent(matcher.group()) : "";
+                percent = matcher.find(end) ? matcher.group() : "";
                 row[Constants.SI_FIELD_PERCENTAGE] = percent;
                 context.addSupplementalInfoRow(row);
                 row[Constants.SI_FIELD_AMOUNT] = getOrdinaryDividendsAmountFormula(context);
@@ -117,34 +117,35 @@ class SupplementalInfoState implements State {
             row[Constants.SI_FIELD_SECURITY_DESCRIPTION] = context.getSecurityDescriptionForNote(note);
             row[Constants.SI_FIELD_SOURCE] = "Fed Source Total";
             matcher = patternPercent.matcher(text);
-            final String percent = matcher.find(end) ? cleanPercent(matcher.group()) : "";
+            final String percent = matcher.find(end) ? matcher.group() : "";
             row[Constants.SI_FIELD_PERCENTAGE] = percent;
             context.addSupplementalInfoRow(row);
             row[Constants.SI_FIELD_AMOUNT] = getOrdinaryDividendsAmountFormula(context);
             return;
         }
-        matcher = patternStartsWithStateAndPercentage.matcher(text);
+        matcher = patternStateAndPercentage.matcher(text);
         if (matcher.find()) {
-            final String[] lastRow = context.getLastSupplementalInfoRow();
+            String[] lastRow = context.getLastSupplementalInfoRow();
             if (!lastRow[Constants.SI_FIELD_SOURCE].isEmpty()) {
                 return;
             }
-            // TODO implement
+            String state;
+            String percentage;
+            while (true) {
+                state = matcher.group(1);
+                percentage = matcher.group(2);
+                lastRow[Constants.SI_FIELD_STATE] = state;
+                lastRow[Constants.SI_FIELD_PERCENTAGE] = percentage + '%';
+                if (!matcher.find()) {
+                    break;
+                }
+                final String[] row = createEmptyRow();
+                row[Constants.SI_FIELD_SECURITY_DESCRIPTION] = lastRow[Constants.SI_FIELD_SECURITY_DESCRIPTION];
+                context.addSupplementalInfoRow(row);
+                row[Constants.SI_FIELD_AMOUNT] = getTaxExemptDividendsAmountFormula(context);
+                lastRow = row;
+            }
         }
-    }
-
-    private static String cleanPercent(final String percent) {
-        final StringBuilder result = new StringBuilder();
-        result.append(percent);
-        if (percent.endsWith("%")) {
-            result.deleteCharAt(result.length() - 1);
-        }
-        result.deleteCharAt(result.length() - 3);
-        while (result.length() < 5) {
-            result.insert(0, '0');
-        }
-        result.insert(1, '.');
-        return result.toString();
     }
 
     private static String[] createEmptyRow() {
